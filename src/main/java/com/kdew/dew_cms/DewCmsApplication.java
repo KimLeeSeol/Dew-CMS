69dab3891a89d06fa84d12330b40b9134a2db7c7
kdew <kdew0308@gmail.com>
2023-05-06 13:09:56 +0900

    Initial commit

---
+++ src/main/java/com/kdew/dew_cms/DewCmsApplication.java
@@ -0,0 +1,13 @@
+package com.kdew.dew_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DewCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DewCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdew/dew_cms/DewCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdew.dew_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DewCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
